e903ee5f342de32e8ea3f99827360b83652f3921
matthew theseira <matt_.t993@hotmail.com>
2024-08-20 13:21:08 +1000

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="max-w-[1440px] mx-auto h-screen">
+      <div className="bg-purple-300">
+        <h1 className="">Contact us, we love to hear from you</h1>
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ThankYou from "./pages/ThankYou.jsx";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/thankyou", element: <ThankYou /> },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
